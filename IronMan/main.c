ee67314823788c70b4164d0ededf23afac4ee432
Petro Rovenskyy <petro.rovenskyy@hotschedules.com>
2017-10-09 11:00:32 +0300

    Initial Commit

---
+++ IronMan/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  IronMan
+//
+//  Created by Petro Rovenskyy on 10/9/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
