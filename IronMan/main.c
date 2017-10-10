d55b94a58863e3c34898ab0cd7790a0f953b6055
Petro Rovenskyy <petro.rovenskyy@hotschedules.com>
2017-10-10 16:27:08 +0300

    Added README.md

--- IronMan/main.c
+++ IronMan/main.c
@@ -12,17 +12,20 @@ MutableArray values;
 
 int minInitialArmor(size_t lenght, int *arr, int minArmorConstraint) {
     initArray(&values, lenght);
-    int armor = 0;
+    int armor = minArmorConstraint;
+    
     for (int i = 0; i< lenght; i++) {
-        int pI = arr[i];
-        int pNext = 0;
-        if ((i + 1) < lenght) {
-            pNext = arr[i + 1];
+        int pI  = arr[i];
+        int pNext = (arr[i+1] < lenght)? arr[i+1] : 0;
+        if (armor + pI < 0) {
+            int armorForBattle = minArmorConstraint - (pI + armor);
+            insertArray(&values, armorForBattle);
         }
-        armor = minArmorConstraint - (pI + pNext);
-        insertArray(&values, armor);
+        if ((armor + (pI + pNext)) <= 0) {
+            armor += minArmorConstraint - (armor + pI + pNext);
+            insertArray(&values, armor);
+        }
     }
-
     qsort(values.array, values.used, sizeof(int), descCmpfunc);
     //DEBUG
     for (int j =0; j < values.used; j++) {
@@ -33,7 +36,12 @@ int minInitialArmor(size_t lenght, int *arr, int minArmorConstraint) {
 }
 
 int main(int argc, const char * argv[]) {
-    int seq[] = { -2, -5, 4, 1, -5, -10 };
+    //int seq[] = { -2, -5, 4, 1, -5, -10 }; => 18
+    //int seq[] = { -5, 4, -2, 3, 1, -1, -6, -1, 0, 5}; // => 8
+    //int seq[]  = {-5, 4, -2, 3, 1}; //=> 6
+    int seq[] = { -5, 4, -2, 3, 1, -1, -6, -1, 0, -5}; // => 13
+    //int seq[]  = {-2, 3, 1, 0, -1}; //=> 3
+    //int seq[]  = { 100, 3, 1, 0, -1}; //=> 3
     size_t lenght = (sizeof(seq) / sizeof(seq[0]));
     int armor = minInitialArmor(lenght, seq, 1);
     printf("Minimum initial armor to survive should be == %ld \n",(long)armor);
