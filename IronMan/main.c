f08cde8e00a24db233ce438ce2f8324cb6c83257
3a4oT <petro.rovenskyy@gmail.com>
2017-10-11 02:38:53 +0300

    working prototype.

--- IronMan/main.c
+++ IronMan/main.c
@@ -8,45 +8,46 @@
 #include <stdio.h>
 #include "MutableArray.h"
 
-MutableArray values;
+MutableArray powerBank;
 
-int minInitialArmor(size_t lenght, int *arr, int minArmorConstraint) {
-    initArray(&values, lenght);
-    int armor = minArmorConstraint;
-    
+int minInitialPower(size_t lenght, int *battleFiled, int minPowerConstraint) {
+    initArray(&powerBank, lenght);
+    int power = minPowerConstraint;
+    insertArray(&powerBank, power);
+    int powerAtLocationI = 0;
     for (int i = 0; i< lenght; i++) {
-        int pI  = arr[i];
-        int pNext = (arr[i+1] < lenght)? arr[i+1] : 0;
-        if (armor + pI < 0) {
-            int armorForBattle = minArmorConstraint - (pI + armor);
-            insertArray(&values, armorForBattle);
-        }
-        if ((armor + (pI + pNext)) <= 0) {
-            armor += minArmorConstraint - (armor + pI + pNext);
-            insertArray(&values, armor);
+        powerAtLocationI += battleFiled[i];
+        int nextLocation = i + 1;
+        int powerAtNextLocation = (nextLocation < lenght) ? battleFiled[nextLocation] : 0;
+        int battle = power + powerAtLocationI;
+        if (battle < 0) {
+            power = minPowerConstraint - (battle);
+            // Really enough to survive in the next location?
+            battle = power + powerAtLocationI + powerAtNextLocation;
+            if (battle <= 0) {
+                power += minPowerConstraint - (battle);
+            }
+            insertArray(&powerBank, power);
         }
     }
-    qsort(values.array, values.used, sizeof(int), descCmpfunc);
-    //DEBUG
-    for (int j =0; j < values.used; j++) {
-        printf("%ld,", (long) values.array[j]);
-    }
-
-    return values.array[0];
+    
+    qsort(powerBank.array, powerBank.used, sizeof(int), descCmpfunc);
+    power = powerBank.array[0];
+    return power;
 }
 
 int main(int argc, const char * argv[]) {
-    //int seq[] = { -2, -5, 4, 1, -5, -10 }; => 18
+    //int seq[] = { -2, -5, 4, 1, -5, -10 }; //=> 18
     //int seq[] = { -5, 4, -2, 3, 1, -1, -6, -1, 0, 5}; // => 8
     //int seq[]  = {-5, 4, -2, 3, 1}; //=> 6
     int seq[] = { -5, 4, -2, 3, 1, -1, -6, -1, 0, -5}; // => 13
     //int seq[]  = {-2, 3, 1, 0, -1}; //=> 3
-    //int seq[]  = { 100, 3, 1, 0, -1}; //=> 3
+    //int seq[]  = { 100, 3, 1, 0, -200}; //=> 97
     size_t lenght = (sizeof(seq) / sizeof(seq[0]));
-    int armor = minInitialArmor(lenght, seq, 1);
-    printf("Minimum initial armor to survive should be == %ld \n",(long)armor);
+    int armor = minInitialPower(lenght, seq, 1);
+    printf("\nMinimum initial power to survive should be == %ld \n",(long)armor);
     
     //Clean up
-    freeArray(&values);
+    freeArray(&powerBank);
     return 0;
 }
